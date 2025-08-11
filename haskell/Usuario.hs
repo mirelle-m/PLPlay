1b390cae01aec497bce4a953ef16855d6143efb6
Mirelle Maria <mirelle.maria.oliveira.rocha@ccc.ufcg.edu.br>
2025-08-11 19:37:09 -0300

    ajustes em Usuario

--- haskell/Usuario.hs
+++ haskell/Usuario.hs
@@ -3,8 +3,11 @@ module Usuario where
 import Data.List
 import System.Directory (doesFileExist)
 import System.IO
-import Text.Read (readMaybe)  
-
+import Text.Read (readMaybe)
+import Data.List (intercalate)
+import Control.Exception (evaluate)
+import Utils (removeAspas, adicionaAspas)
+import Data.List.Split (splitOn)
 
 data Usuario = Usuario
   { nomeUsuario :: String
@@ -12,24 +15,11 @@ data Usuario = Usuario
   , progressoUsuario :: String
   } deriving (Show)
 
-splitOn :: Char -> String -> [String]
-splitOn delimiter = foldr f [[]]
-  where
-    f c (h:t)
-      | c == delimiter = []:h:t
-      | otherwise = (c:h):t
-
 salvarUsuario :: Usuario -> IO ()
 salvarUsuario usuario = do
     let caminho = "../data/usuarios.csv"
-    -- let cabecalho = "nome,senha,progresso"
-    let dadosUsuario = (nomeUsuario usuario) ++ "," ++ (senhaUsuario usuario) ++ "," ++ (progressoUsuario usuario)
+    let dadosUsuario = intercalate "," [nomeUsuario usuario, senhaUsuario usuario, progressoUsuario usuario]
     appendFile caminho (dadosUsuario ++ "\n")
-
-removeAspas :: String -> String
-removeAspas str
-  | length str >= 2 && head str == '"' && last str == '"' = init (tail str)
-  | otherwise = str
 
 carregaUsuario :: String -> IO (Maybe Usuario)
 carregaUsuario userName = do
@@ -38,25 +28,26 @@ carregaUsuario userName = do
         then return Nothing
         else do
             conteudo <- readFile "../data/usuarios.csv"
+            _ <- evaluate (length conteudo)
             let linhas = drop 1 (lines conteudo)
-                usuarios = map (map removeAspas . splitOn ',') linhas
-                usuarioEncontrado = 
+                usuarios = map (map removeAspas . splitOn ",") linhas
+                usuarioEncontrado =
                     case filter (\cols -> not (null cols) && head cols == userName) usuarios of
                         (cols:_) | length cols >= 3 -> Just (Usuario (cols !! 0) (cols !! 1) (cols !! 2))
                         _ -> Nothing
             return usuarioEncontrado
-  
+
 atualizaLoginAtual :: String -> IO ()
 atualizaLoginAtual nomeUsuario = do
     let caminho = "../data/loginAtual.txt"
     writeFile caminho (nomeUsuario ++ "\n")
 
 recuperaLoginAtual :: IO String
-recuperaLoginAtual = readFile "../data/loginAtual.txt"
+recuperaLoginAtual = do
+    conteudo <- readFile "../data/loginAtual.txt"
+    _ <- evaluate (length conteudo)
+    return (filter (/= '\n') conteudo)
 
-
-addAspas :: String -> String
-addAspas s = "\"" ++ s ++ "\""
 
 atualizaProgresso :: String -> IO Bool
 atualizaProgresso novoValor = do
@@ -66,6 +57,7 @@ atualizaProgresso novoValor = do
         then return False
         else do
             conteudo <- readFile caminho
+            _ <- evaluate (length conteudo)
             let linhas = lines conteudo
             if null linhas
                 then return False
@@ -80,7 +72,7 @@ atualizaProgresso novoValor = do
 
 atualizarSeNomeBater :: String -> String -> String -> String
 atualizarSeNomeBater nome novoValor linha =
-    let colunas = map removeAspas (splitOn ',' linha)
+    let colunas = map removeAspas (splitOn "," linha)
     in if not (null colunas) && head colunas == nome && length colunas >= 3
-        then intercalate "," [addAspas (colunas !! 0), addAspas (colunas !! 1), addAspas novoValor]
+        then intercalate "," [adicionaAspas (colunas !! 0), adicionaAspas (colunas !! 1), adicionaAspas novoValor]
         else linha
