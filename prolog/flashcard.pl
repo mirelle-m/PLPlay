843fc15b3fcd811de3eb2bd910ddb92410173860
Mirelle Maria <mirelle.maria.oliveira.rocha@ccc.ufcg.edu.br>
2025-09-14 13:53:39 -0300

    feat: adiciona formatação para o modo treino

--- prolog/flashcard.pl
+++ prolog/flashcard.pl
@@ -1,43 +1,72 @@
-:- module(flashcards, [iniciar_treino/0]).
+:- module(flashcard, [iniciar_treino/0]).
 
 :- use_module(library(readutil)).
 :- use_module(library(random)).
-
-:- consult(flashcards).
+:- use_module(utils, [
+    limpar_tela/0,
+    terminal_largura/1,
+    mostrar_banner/1,
+    centralizar/3,
+    linha_sep/2
+]).
+:- use_module('../data/flashcards').
 
 carregar_flashcards(Flashcards) :-
-    findall(flashcard(P, R), flashcard(P, R), Flashcards).
+    findall(flashcard(P,R), flashcards:flashcard(P,R), Flashcards).
 
 escolher_perguntas_aleatorias(N, Lista, Selecionados) :-
     random_permutation(Lista, Embaralhados),
-    length(Selecionados, N),
+    length(Embaralhados, Tam),
+    Min is min(N, Tam),
+    length(Selecionados, Min),
     append(Selecionados, _, Embaralhados).
 
-mostrar_flashcards([]).
-mostrar_flashcards([flashcard(P, R)|Fs]) :-
-    nl, write("===================================="), nl,
-    write("Pergunta: "), write(P), nl, nl,
-    write("Pressione Enter para ver a resposta..."), nl,
+mostrar_flashcards([]) :-
+    write("Você chegou ao fim dos flashcards!"), nl,
+    menu_fim_treino.
+
+mostrar_flashcards([flashcard(P,R)|Fs]) :-
+    limpar_tela,
+    terminal_largura(Largura),
+    linha_sep(Largura, Linha),
+    writeln(Linha),
+    centralizar(Largura, P, PerguntaC),
+    nl, write(PerguntaC), nl, nl,
+    linha_sep(Largura, Linha),
+    writeln(Linha),
+    write("\nAperte Enter para ver a resposta\n"),
     read_line_to_string(user_input, _),
-    write("Resposta: "), write(R), nl,
-    write("===================================="), nl, nl,
-    write("Digite 'sair' para encerrar ou Enter para continuar: "), nl,
+    linha_sep(Largura, Linha),
+    writeln(Linha),
+    centralizar(Largura, R, RespostaC),
+    nl, write(RespostaC), nl, nl,
+    linha_sep(Largura, Linha),
+    writeln(Linha),
+    write("\nDigite 'sair' para encerrar o treino ou apenas Enter para continuar"),
     read_line_to_string(user_input, Opcao),
-    ( string_lower(Opcao, "sair") -> write("Treino encerrado!"), nl; 
-    mostrar_flashcards(Fs)).
+    ( string_lower(Opcao, "sair") ->
+        write("Treino encerrado!"), nl,
+        menu_fim_treino;
+        mostrar_flashcards(Fs) ).
 
 iniciar_treino :-
+    limpar_tela,
+    mostrar_banner('../banners/inicio_treino.txt'), nl,
+    write("Pressione Enter para começar..."), nl,
+    read_line_to_string(user_input, _),
     carregar_flashcards(Todos),
-    (Todos == [] -> write("Nenhum flashcard carregado!"), nl;
-    escolher_perguntas_aleatorias(10, Todos, Selecionados),
-        mostrar_flashcards(Selecionados),
-        menu_fim_treino).
+    ( Todos == [] -> 
+        write("Nenhum flashcard carregado!"), nl,
+        menu:menu_principal; 
+        escolher_perguntas_aleatorias(10, Todos, Selecionados),
+        mostrar_flashcards(Selecionados) ).
 
 menu_fim_treino :-
-    nl, write("=== Fim do treino ==="), nl,
+    limpar_tela,
+    mostrar_banner('../banners/fim_treino.txt'), nl,
     write("1 - Treinar novamente"), nl,
     write("2 - Voltar ao menu"), nl,
     read_line_to_string(user_input, Opcao),
-    (Opcao = "1" -> iniciar_treino; 
-    Opcao = "2" -> write("Voltando ao menu..."), nl;
-    write("Opção inválida."), nl, menu_fim_treino).
+    ( Opcao = "1" -> iniciar_treino; 
+        Opcao = "2" -> menu:menu_principal;
+     write("Opção inválida."), nl, menu_fim_treino ).
